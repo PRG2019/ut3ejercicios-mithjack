76e85bf9fb7c5312ffbaa0684ecc63ec52df5e3f
mithjack <alexlopez168@hotmail.com>
2019-11-16 12:02:48 +0100

    Update Main.java

--- 3Ejecicios/src/ej6/Main.java
+++ 3Ejecicios/src/ej6/Main.java
@@ -8,32 +8,32 @@ public static void main(String[]args) {
 	
 	System.out.println("Introduceme un peso");
 	peso=sc.nextDouble();
-	//System.out.println("Dime su unidad:\nLb, Li, Oz, P, K, G, Q");
 	unidad="K";
-	Peso a=new Peso(peso,unidad);
+	Peso a1=new Peso(peso,unidad);
 	boolean sw2=false;
 	while(!sw2) {
 	System.out.println("");
-	System.out.println("Actualmente "+a.mPeso()+a.mUnidad());
+	System.out.println("Actualmente "+a1.mPeso()+a1.mUnidad());
 	System.out.println("Que quieres hacer?");
 	System.out.println("1. Pasar a Libras");
 	System.out.println("2. Pasar a Lingotes");
-	System.out.println("3. Pasar a otra unidad\nLb, Li, Oz, P, K, G, Q");
+	System.out.println("3. Pasar a otra unidad");
 	int men=sc.nextInt();
 	switch(men) {
 	case 1:
-		a.getLibras();
-		System.out.println(a.mPeso());
+		a1.getLibras();
+		System.out.println(a1.mPeso());
 		break;
 	case 2:
-		a.getLingotes();
-		System.out.println(a.mPeso());
+		a1.getLingotes();
+		System.out.println(a1.mPeso());
 		break;
 	case 3:
 		System.out.println("A que unidad Quieres pasarlo\nLb, Li, Oz, P, K, G, Q");
 		unidad=sc.next();
-		a.getPeso(unidad);
-		System.out.println(a.mPeso()+a.mUnidad());
+		//a.getPeso(unidad);
+		//System.out.println(a.mPeso()+a.mUnidad());
+		System.out.println("Son: "+ a1.getPeso(unidad)+" "+unidad);
 		break;
 	default:
 		System.out.println("Opcion invalida");
