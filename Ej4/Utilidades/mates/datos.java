61c8f4e9690fb60e2118193e97c952a1ff3f274f
mithjack <alexlopez168@hotmail.com>
2019-12-19 09:53:30 +0100

    Add files via upload

---
+++ Ej4/Utilidades/mates/datos.java
@@ -0,0 +1,11 @@
+package Utilidades.mates;
+
+class datos {
+int num1;
+int num2;
+	
+datos(int a, int b){
+	num1=a;
+	num2=b;
+	}
+}
---
+++ Ej4/Utilidades/mates/potenciar.java
@@ -0,0 +1,17 @@
+package Utilidades.mates;
+
+public class potenciar extends datos {
+	
+	
+	
+	
+	potenciar(int num1, int num2) {
+		super(num1, num2);
+	}
+
+		//metodos
+	public int darPotencia(){
+		return (int) Math.pow(num1, num2);
+		
+	}
+}
