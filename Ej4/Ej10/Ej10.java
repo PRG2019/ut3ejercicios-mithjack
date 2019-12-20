cf4c38027e3e26e2888e4a26b94e89d303c173ab
mithjack <alexlopez168@hotmail.com>
2019-12-20 09:31:46 +0100

    Add files via upload

--- Ej4/Ej10/Ej10.java
+++ Ej4/Ej10/Ej10.java
@@ -14,16 +14,25 @@ public class Ej10 {
 	else rdo=decbinbas(x/b,b)*10+x%b;
 	return rdo;
 }
+	
 	public static String stdecbin(int x) {
 		if (x<2)  st+=x;
 		else st+=stdecbin(x/2)+x%2;
 		return st;
 }
-	public static String stdecbinbas(int x, int b) {
+	/*public static String stdecbinba(int x, int b) {
+	
 		if (x<b)  st+=x;
 		else st+=stdecbinbas(x/b,b)+x%b;
 		return st;
-}
+	}*/
+	
+	public static String stdecbinbas(int x, int b) {
+		 //String st="";
+			if (x<b)  st=st + (char) ( (x<10)? x+'0' :  (x-10+'A'));
+			else st+=stdecbinbas(x/b,b) + (char) ((x%b<10)? x%b+'0' : x%b-10+'A');
+			return st;
+	}
 	
 public static void main(String[]args) {
 	Scanner sc=new Scanner(System.in);
@@ -37,26 +46,26 @@ public static void main(String[]args) {
 	case 1:
 		System.out.println("numero");
 		int num=sc.nextInt();
-		System.out.println(Ej10.decbin(num));
+		System.out.println(decbin(num));
 		break;
 	case 2:
 		System.out.println("numero");
 		num=sc.nextInt();
 		System.out.println("base");
 		int bas=sc.nextInt();
-		System.out.println(Ej10.decbinbas(num,bas));
+		System.out.println(decbinbas(num,bas));
 		break;
 	case 3:
 		System.out.println("numero");
 		num=sc.nextInt();
-		System.out.println(Ej10.stdecbin(num));
+		System.out.println(stdecbin(num));
 		break;
 	case 4:
 		System.out.println("numero");
 		num=sc.nextInt();
 		System.out.println("base");
 		bas=sc.nextInt();
-		System.out.println(Ej10.stdecbinbas(num,bas));
+		System.out.println(stdecbinbas(num,bas));
 		break;
 		
 	}
