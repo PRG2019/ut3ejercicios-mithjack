8ff9a18460fd4d8849434d56e3af77c50cefb50b
mithjack <alexlopez168@hotmail.com>
2020-01-07 13:37:23 +0100

    Update Ej14.java

--- Ej4/Ej14/Ej14.java
+++ Ej4/Ej14/Ej14.java
@@ -1,11 +1,12 @@
 //@mithjack
 package Ej14;
 /*Implemente un subprograma que halle cual es la primera potencia 
- * en base 2 mayor que un número que pasamos como parámetro, 
- * devolviendo el valor de dicha potencia y el exponente al que está elevado.*/
+ * en base 2 mayor que un nÃºmero que pasamos como parÃ¡metro, 
+ * devolviendo el valor de dicha potencia y el exponente al que estÃ¡ elevado.*/
 import java.util.*;
 public class Ej14 {
 
+	//metodo 1
 	static int potenciaDeDos(int n, int[]x) {
 	int rdo=0;
 	if (Math.pow(2,x[0])>n) rdo=1;
@@ -15,6 +16,16 @@ public class Ej14 {
 	}
 	return rdo;
 	}
+	
+	//metodo 2
+	static int potenciaDeDos2(int n) {
+		int rdo=0;
+		if (n<=0) rdo=1;
+		else {
+			rdo=2*potenciaDeDos2(n/2);
+		}
+		return rdo;
+		}
 	
 	
 	public static void main(String[]args) {
@@ -28,8 +39,11 @@ public class Ej14 {
 		
 		potencia=potenciaDeDos(n,x);
 		System.out.println("La potencia que buscas es "+x[0]);
+		potencia=potenciaDeDos2(n);
+		System.out.print("El exponente es: ");
+		System.out.println(potencia=potenciaDeDos2(n));
+		
 		System.out.println("2^"+x[0]+"="+potencia+">"+n);
-
 		sc.close();
 	}
 }
