c92313da7f0405225f11e13d8b3d69132c54d8f0
mithjack <alexlopez168@hotmail.com>
2020-01-13 11:29:33 +0100

    Add files via upload

---
+++ Ej5/EJ1/Ej1.java
@@ -0,0 +1,28 @@
+package EJ1;
+
+import java.util.Scanner;
+
+public class Ej1 {
+	public static Scanner sc=new Scanner(System.in);
+	public static final int N=51;
+	
+	public static void main(String[]args) {
+		int idv=sc.nextInt();
+		int[]vec=new int[idv];
+		rellenar(vec);
+		System.out.println("Dime numero a buscar de 0 a 50");
+		int num=sc.nextInt();
+		numeros(vec,num);
+	}
+	
+	public static void rellenar(int[]vec) {
+		for (int i=0;i<N;i++) {
+			vec[i]=i;	
+		}
+	}
+	public static void numeros (int[] vec,int num) {
+		for (int i=num;i<N;i++) {
+			System.out.println(vec[i]);
+		}
+	}
+}
