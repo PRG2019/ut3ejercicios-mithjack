9db63ff2b3a1c448bd123953c9fff1f24654908a
mithjack <alexlopez168@hotmail.com>
2020-03-31 14:12:48 +0200

    Create TestForma.java

---
+++ EJ6/Ej7/TestForma.java
@@ -0,0 +1,30 @@
+```java
+
+/*
+*@mithjack
+*/
+
+class TestForma{
+
+public static void main (String [] args){
+Forma f= new Circulo();
+f.identidad();
+
+Circulo c=new Circulo();
+((Forma)c).identidad(); //((Forma.C).identidad(); La C estaba en mayuscula, y lo declaramos como minuscula.
+/*((Circulo)f).identidad(); // El metodo identidad esta en Forma, hacer un downcasting a Circulo no serviria, 
+* ya que no está el metodo ahí.
+*/
+Forma f2 = new Circulo(); //Forma f2 = new Forma(); No se puede instanciar Forma, lo cambiamos a Circulo.
+f2.identidad();
+/*((Forma)f).identidad(); //(forma)f.identidad(); La f tiene que ser en mayusculas al igual que la Clase que 
+*representa, pero aun así f ya es de Forma (Forma f= new Circulo();) no se le puede hacer ni downcasting por 
+*lo que habria que eliminar la linea.
+*/
+}
+}
+
+/*
+* En pantalla aparecerá el identificador de f, luego de c  de f2.
+*/
+
